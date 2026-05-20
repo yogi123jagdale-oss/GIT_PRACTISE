2c45774a15f98115374cdc7c320594c667f4301c
yogesh-git-practise <yogi123jagdale@gmail.com>
2026-05-21 02:07:59 +0530

    Added Memory consumption on new_branch

--- system_monitoring.sh
+++ system_monitoring.sh
@@ -7,3 +7,5 @@ LOG_FILE="./log/system_health_check.log"
 echo "================================== SYSTEM REPORT ============================" >> $LOG_FILE
 uptime >> $LOG_FILE
 
+echo "================================== Top 5 Memory consuming processes ============================" >> $LOG_FILE
+ps -aux --sort=-%mem | head -5 >> $LOG_FILE
